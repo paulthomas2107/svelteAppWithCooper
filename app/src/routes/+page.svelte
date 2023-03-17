9406dd3b3361b856f7a002e8fe412ebb8a3f05dc
Paul Thomas <paulthomas2107@gmail.com>
2023-03-17 10:57:15 +0000

    More

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,23 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<h1>Let's get cracking bones!</h1>
+<div class="flex justify-center">
+	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+		<h3 class="text-center py-6">Create A Code Snippet</h3>
+		<div class="card p-4 w-full text-token space-y-4">
+			<label class="label">
+				<span>Snippet Title</span>
+				<input class="input" type="text" placeholder="Enter Title Here..." />
+			</label>
+			<label class="label">
+				<span>Programmimg Language</span>
+				<select class="select">
+					<option value="html">HTML</option>
+					<option value="css">CSS</option>
+					<option value="typescript">TypeScript</option>
+				</select>
+			</label>
+			<label class="label">
+				<span>Code Snippet</span>
+				<textarea class="textarea" rows="4" placeholder="Enter your snippet code here..." />
+			</label>
+		</div>
+	</div>
 </div>
